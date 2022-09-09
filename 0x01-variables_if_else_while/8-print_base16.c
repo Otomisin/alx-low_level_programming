a970a2594141f546fa41fe994e0e68807543aa54
otomisin <orenaiket@yahoo.com>
2022-09-09 20:40:00 +0100

    new

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
- */
+* main - print quote
+*
+* Description: prints Dora's quote
+*
+* Return: Always 1 (Success)
+*/
+
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-	putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-	putchar(c);
-}
-putchar('\n');
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
