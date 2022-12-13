9fe4945ceecb6d1fdce451b243b241820b06bea9
Otomisin <orenaiket@yahoo.com>
2022-12-13 18:48:05 +0100

    hope

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"); // THe difference between printf() and puts() is that the printf() function is used to print both strings and variables to the screen while the puts() function only permits you to print a string only to your screen.
 	return (0);
 }
