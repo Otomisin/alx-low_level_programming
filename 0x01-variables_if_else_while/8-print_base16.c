dfc0a00f2fd8ecdde914f9b7c44f6ad3bb468f41
Otomisin <orenaiket@yahoo.com>
2022-12-14 10:15:34 +0100

    clean

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
  * main - prints all hex numbers in lowercase
- * Objectives: Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.
+ * Objectives: Write a program that prints all
+ * the numbers of base 16 in lowercase, followed by a new line.
  * Return: returns 0 if successful
  */
 int main(void)
 {
-	int a;
-	char ch;
+int a;
+char ch;
 
-	for (a = 48; a < 58; a++)
-	{
-		putchar(a);
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
